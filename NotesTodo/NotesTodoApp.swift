14831906a082a909e34e2e1c196c7584ed84a8ac
baddazi <48481706+baddazi@users.noreply.github.com>
2023-02-27 20:29:13 +0100

    Initial Commit

---
+++ NotesTodo/NotesTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesTodoApp.swift
+//  NotesTodo
+//
+//  Created by David Záruba on 27.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct NotesTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
